d4d7a1a8a5d793d17c3bc37cce47ae1f38116c98
jonathancg90 <jonathancg90@gmail.com>
2013-09-21 15:26:18 -0500

    Update changes

---
+++ hackathon/apps/HistoryPlay/models/Answer.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Answer(models.Model):
+    name = models.CharField (
+        verbose_name=_(u'respuesta'),
+        max_length=150,
+    )
+    answer = models.ForeignKey (
+        'Answer',
+        verbose_name=_(u'Pregunta'),
+        related_name='answer_set'
+    )
+    image = models.CharField (
+        verbose_name=_(u'respuesta'),
+        max_length=150,
+    )
+    is_correct = models.BooleanField (
+        editable=False,
+        default=False
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+        editable=False
+    )
+
+    def __unicode__(self):
+        return self.street[:30]
+
+    class Meta:
+        app_label='hp'
---
+++ hackathon/apps/HistoryPlay/models/Question.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+
+class Question(models.Model):
+    name = models.CharField(
+        verbose_name=_(u'Pregunta'),
+        max_length=150,
+    )
+    created = models.DateTimeField(
+        auto_now_add=True,
+        editable=False
+    )
+    modified = models.DateTimeField(
+        auto_now=True,
+        editable=False
+    )
+
+    def __unicode__(self):
+        return self.street[:30]
+
+    class Meta:
+        app_label='hp'
