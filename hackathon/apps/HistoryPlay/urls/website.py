4ed02a7d08fcedf836a59b3eb24c767934159ec3
jonathancg90 <jonathancg90@gmail.com>
2013-09-21 14:36:15 -0500

    Base project templates

---
+++ hackathon/apps/HistoryPlay/urls/website.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from django.conf.urls import url, patterns
+from apps.HistoryPlay.views import home as h
+
+urlpatterns = patterns('',
+    url(r'^$', h.HomeVIe.as_view(),
+        name='home'),
+)
