f16d73d33d8fd304091fbed441b2225b022e697e
John Machahuay <john.cfmr.2009@gmail.com>
2013-09-22 08:56:25 -0500

    Market in the map

--- hackathon/hackathon/settings.py
+++ hackathon/hackathon/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'historyplay',                      # Or path to database file if using sqlite3.
+        'NAME': 'app_john',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
         'PASSWORD': '123',
