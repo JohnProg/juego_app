1b8128fce18080b306daf849c07441d411abe2a5
John Machahuay <john.cfmr.2009@gmail.com>
2013-09-21 22:05:47 -0500

    Styles in the map template

--- hackathon/apps/HistoryPlay/urls/website.py
+++ hackathon/apps/HistoryPlay/urls/website.py
@@ -6,6 +6,6 @@ from apps.HistoryPlay.views import home as h
 urlpatterns = patterns('',
     url(r'^$', h.HomeVIe.as_view(),
         name='home'),
-    url(r'^map$', h.MapRoute.as_view(),
+    url(r'^map/$', h.MapRoute.as_view(),
         name='map'),
 )
--- hackathon/apps/HistoryPlay/views/home.py
+++ hackathon/apps/HistoryPlay/views/home.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 class HomeVIe(TemplateView):
     template_name = 'home.html'
 
+
 class MapRoute(TemplateView):
     template_name = 'map.html'
