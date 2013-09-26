08ee2c88841636695406da28c309ceb57da6dc93
Jonathancg <Jonathancg90@gmail.com>
2013-09-26 15:29:47 -0400

    update

--- lima360/wsgi.py
+++ lima360/wsgi.py
@@ -23,12 +23,12 @@ application = Cling(get_wsgi_application())
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
 # os.environ["DJANGO_SETTINGS_MODULE"] = "lima360.settings"
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lima360.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lima360.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
+
 # application = get_wsgi_application()
 
 # Apply WSGI middleware here.
