1207400a5f583d397333adc2cfb9541edf541ce8
Jonathancg <Jonathancg90@gmail.com>
2013-09-26 16:04:21 -0400

    Update

--- lima360/settings.py
+++ lima360/settings.py
@@ -8,16 +8,6 @@ ROOT_PATH = realpath(join(dirname(__file__), '..'))
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
-#
-#
-#
-# import os
-# PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-#
-# STATICFILES_DIRS = (
-#     os.path.join(PROJECT_PATH, 'static'),
-# )
-
 
 MANAGERS = ADMINS
 
@@ -76,27 +66,19 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-# STATICFILES_DIRS = (
-#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
-#     # Always use forward slashes, even on Windows.
-#     # Don't forget to use absolute paths, not relative paths.
-#     join(ROOT_PATH, 'static'),
-#     join(ROOT_PATH, 'media')
-# )
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    # Put strings here, like "/home/html/static" or "C:/www/django/static".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    join(ROOT_PATH, 'static'),
+    join(ROOT_PATH, 'media')
 )
 
 # List of finder classes that know how to find static files in
--- lima360/urls.py
+++ lima360/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
@@ -7,3 +8,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
      url(r'^',  include('apps.HistoryPlay.urls.website')),
 )
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
