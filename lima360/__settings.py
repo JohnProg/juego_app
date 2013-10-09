43d939e18245140a79fa0a2fb94028ebac3d411c
Jonathancg <Jonathancg90@gmail.com>
2013-10-09 03:23:41 -0400

    Config settings

--- lima360/__settings.py
+++ lima360/__settings.py
@@ -4,6 +4,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 PROJECT_ROOT = realpath(join(dirname(__file__), '../..'))
 ROOT_PATH = realpath(join(dirname(__file__), '..'))
+print(ROOT_PATH)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -13,11 +14,11 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lima360',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '123456',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
--- lima360/_heroku_settings.py
+++ lima360/_heroku_settings.py
@@ -15,12 +15,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'lima360',                      # Or path to database file if using sqlite3.
+        'NAME': 'd3ss2l9v1ggfrb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '123',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'USER': 'apssgwcriinpmd',
+        'PASSWORD': 'HWUk9JAJM7wLJY09KHDM-rUbRO',
+        'HOST': 'ec2-54-227-251-13.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PORT': '5432',                      # Set to empty string for default.
     }
 }
 
