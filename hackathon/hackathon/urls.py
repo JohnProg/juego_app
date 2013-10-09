91ea548b045b3b4ac39a51048391856bea5b92b1
Jonathancg <Jonathancg90@gmail.com>
2013-10-09 02:51:23 -0400

    Reestructure

--- hackathon/hackathon/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
-urlpatterns = patterns('',
-     url(r'^',  include('apps.HistoryPlay.urls.website')),
-)
