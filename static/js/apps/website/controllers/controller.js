251de54107f332fcadcb629c040fc72ae0219e9a
Jonathancg <Jonathancg90@gmail.com>
2013-10-12 14:59:35 -0400

    add controller website

--- static/js/apps/website/controllers/controller.js
+++ static/js/apps/website/controllers/controller.js
@@ -1,7 +1,43 @@
 var controllers = {};
 
-controllers.websiteController = function($scope){
-    $scope.temp = 'Hola';
+controllers.websiteController = function($scope, $http, userUrl){
+    $scope.login = function(username, password){
+        var url = userUrl.loginUrl;
+        var data = {
+          username: username,
+          password: password
+        };
+
+        return $http.post(url, data).success(function(response, status){
+            if(status == 200){
+
+            }
+        });
+    };
+    $scope.signup = function(username, email, password){
+        var url = userUrl.signupUrl;
+        var data = {
+          username: username,
+          email: email,
+          password: password
+        };
+
+        return $http.post(url, data).success(function(response, status){
+            if(status == 200){
+
+            }
+        });
+    };
+    $scope.logout = function(){
+        var url = userUrl.logoutUrl;
+
+        return $http.get(url).success(function(response, status){
+            if(status == 200){
+
+            }
+        });
+    };
+
 };
 
 websiteapp.controller(controllers);
